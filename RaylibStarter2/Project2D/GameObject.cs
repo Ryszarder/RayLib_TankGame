43e8d1a58171f4d1729067d97b2bd43b279890aa
Blake Peck <s210561@students.aie.edu.au>
2021-03-22 12:24:32 +1100

    Setting out the Protected Variables and Functions for the GameObject expect for the 'Raduis for collision; Protected Variable

--- RaylibStarter2/Project2D/GameObject.cs
+++ RaylibStarter2/Project2D/GameObject.cs
@@ -3,12 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raylib;
+using static Raylib.Raylib;
 using MathClasses;
 
 namespace Project2D
 {
 	class GameObject
 	{
-		
+		protected GameObject parent = null;
+		protected List<GameObject> m_Children = new List<GameObject>();
+		protected Matrix3 localTransform = new Matrix3();
+		protected Matrix3 globalTransform = new Matrix3();
+		protected Image m_Sprite;
+		protected Texture2D m_Texture;
+
+
+		public void SetParent()
+		{
+
+		}
+
+		public void GetParent()
+		{
+
+		}
+
+		public void AddChild()
+		{
+
+		}
+
+		public void RemoveChild()
+		{
+
+		}
+
+		public void SetPosition()
+		{
+
+		}
+
+		public void GetPosition()
+		{
+
+		}
+
+		public void SetScale()
+		{
+
+		}
+
+		public void GetScale()
+		{
+
+		}
+
+		public void Update()
+		{
+
+		}
+
+		public void UpdateTransforms()
+		{
+
+		}
+
+		public void Draw()
+		{
+
+		}
+
+		public void OnCollision()
+		{
+
+		}
+
+		public void GetRadius()
+		{
+
+		}
+
 	}
 }
